d49adba2febba7908380f7fe551db0eeebbe7003
danielanthonyl <danskyjs@icloud.com>
2023-09-30 22:29:30 -0300

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by daniel anthony on 30/09/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
